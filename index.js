419c502cfef4207a38cba499d1d2f42105e716d2
Mukhesh-Sompalli <mukheshsompalli02@gmail.com>
2024-08-25 13:34:43 +0530

    add

---
+++ index.js
@@ -0,0 +1,119 @@
+// let x=[1,1,2,2,3,4,5]
+// let y=[6,4,3,7,8,9]
+// let z=[0,8,7,6,5,4]
+// let res=x.concat(y,z)
+// console.log(res)
+
+
+
+
+// const arr=[[1,2,3,4],[1,2,3,4]]
+// const res=arr.flat()
+// console.log(res)
+
+
+
+
+// let x=[10,20,30,40,50]
+// let y=[60,70,80,90]
+// y.pop(3);
+// console.log(y)
+
+
+
+// -----------------task-----------
+// join() 
+
+// let x=["Hello","world","this","is","javascript"]
+// let res=x.join()
+// console.log(res)
+
+// ---------------------------------------
+// delete
+
+// let num=[1,2,3,4,5]
+// let res=delete(num)
+// console.log(res)
+// ------------------------------------------
+// concat
+
+// let arr1=[1,2,3]
+// let arr2=[4,5,6]
+// let res=arr1.concat(arr2)
+// console.log(res)
+
+
+// -------------------------
+// flat
+
+// let array=[[1,2],[3,4],[5,[6,7]]]
+// let res=array.flat().flat()
+// console.log(res)
+
+
+// -----------------------------------
+// splice
+
+// let num=["apple","banana","orange","grape"]
+// let res=num.splice(0,2,"kiwi","mango")
+// console.log(res)
+// --------------------------
+// slice
+
+
+// let num=[1,2,3,4,5,6,7,8]
+// let res=num.slice(2,5)
+// console.log(res)
+
+
+
+
+
+
+// let array=[1,2,3,4,5]
+// let z=array.map(z=> z*2 )
+// console.log(z)
+
+// let x=[1,2,3,4,5]
+// c=0
+// let x=["hello","world","hellow"]
+// let z=x.filter(z=>  c++,x>=5)
+// console.log(z)
+
+
+// let x=[1,2,3,4,5]
+// let z=x.forEach(x=> {return console.log(x)}   
+// )
+
+// let arr=[1,2,3,4,5,6]
+// let y=arr.map((x,y,z)=>{return `${x},${y},${z}`})
+// console.log(y)
+
+
+// let arr=[1,2,3,4,5,6,7]
+// let z=arr.map(function(x)
+// {
+//     return x;
+   
+// }
+
+// )
+// console.log(arr)
+
+
+
+
+//  let y=["hello","hi","mukesh","kumar/","from","tpt"]
+// let z1=y.map(function(y1){
+// return y1.toUpperCase()
+// }
+// )
+
+let y=[1,3,4,5]
+let z1=y.findIndex(num=>y%2==0)
+console.log(z1)
+
+
+
+
+
